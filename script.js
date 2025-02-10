ae798454f0000fe0d100f0019ef37aa85e34fd6c
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-10 17:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+	let character=s2.charAt(0);
+
+	for(let i=0;i<s1.length;i++){
+		if(s1.charAt(i)==character){
+			return i;
+		}
+	}
+	return -1;
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
